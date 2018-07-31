f27f2bdc9a82e577f4108ff3f22d837befad5930
firecoded <yepyepguru@gmail.com>
2018-07-31 09:55:17 -0700

    completed grade table

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,25 @@
+import React from 'react';
+import Table from "./table";
+
+
+const students = [
+		{name: "Jeff Goldblum",
+		course: "Dinosaur Genetics",
+		grade: "88"},
+		{name: "Joe Dirt",
+		course: "Economics",
+		grade: "98"},
+		{name: "John Jacob",
+		course: "Stealing names",
+		grade: "86"}
+];
+
+export default () => {
+    return (<div className = 'container'>
+    	<h1>Student Grade Table</h1>
+    	<Table data={students}/>
+    </div>)
+};
+
 
 
