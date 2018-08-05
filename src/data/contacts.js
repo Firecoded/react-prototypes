27417009db80cc30b81c7259eeff279aa12811ed
firecoded <yepyepguru@gmail.com>
2018-08-05 16:52:07 -0700

    protoype completed

---
+++ src/data/contacts.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        firstName: 'Jim',
+        lastName: 'Bob',
+        phone: '785-328-9020',
+        email: 'superman@google.com'
+    },
+    {
+        firstName: 'Jim',
+        lastName: 'Bo',
+        phone: '201-725-5692',
+        email: 'theRealBatman@google.com'
+    },
+    {
+        firstName: 'Bob',
+        lastName: 'Jim',
+        phone: '319-210-7259',
+        email: 'theFlash@google.com'
+    }
+];
